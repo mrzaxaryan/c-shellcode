11bc12fef26f3628df90fc7209e88d81d45a6402
admin <admin>
2025-11-28 11:45:43 +0400

    Refactor: reorder build commands to prioritize Linux compilation; improve script organization

--- build.bat
+++ build.bat
@@ -1,13 +1,22 @@
 @echo off
 setlocal enabledelayedexpansion
 
-call compile.bat i386-pc-windows-msvc src\*.c 0x400000
+call compile-linux.bat i386-pc-linux-gnu src\*.c
 if errorlevel 1 goto :end
 
-call compile.bat x86_64-pc-windows-msvc src\*.c 0x140000000
+call compile-linux.bat x86_64-pc-linux-gnu src\*.c
 if errorlevel 1 goto :end
 
-call compile.bat aarch64-pc-windows-msvc src\*.c 0x0000000140000000
+call compile-linux.bat aarch64-pc-linux-gnu src\*.c
+if errorlevel 1 goto :end
+
+call compile-windows.bat i386-pc-windows-msvc src\*.c 0x400000
+if errorlevel 1 goto :end
+
+call compile-windows.bat x86_64-pc-windows-msvc src\*.c 0x140000000
+if errorlevel 1 goto :end
+
+call compile-windows.bat aarch64-pc-windows-msvc src\*.c 0x0000000140000000
 if errorlevel 1 goto :end
 
 :end
